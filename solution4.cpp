7bfa2d0041538cfe9fa96aa2c71643a5f55bc800
lornagloria <lornagloria0@gmail.com>
2025-04-22 08:36:43 +0100

    Add files via upload

--- solution4.cpp
+++ solution4.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 using namespace std;
 
 class DataBundle {
@@ -6,51 +7,43 @@ private:
     int dataVolume;
 
 public:
-    DataBundle() : dataVolume(500) {
-        cout << "Default data bundle of 500MB activated." << endl;
+    DataBundle() {
+        dataVolume = 500;
     }
-
-    DataBundle(int volume) : dataVolume(volume) {
-        cout << " data bundle of " << dataVolume << "MB activated." << endl;
-    }
+    DataBundle(int initialVolume) : dataVolume(initialVolume) {}
 
     void checkData() {
-        cout << "Remaining data: " << dataVolume << "MB" << endl;
-    }
-
-    void useData(int amount) {
-        if (amount <= dataVolume && amount > 0) {
-            dataVolume -= amount;
-            cout << amount << "MB of data used." << endl;
-        } else if (amount <= 0) {
-            cout << "Invalid data usage amount." << endl;
-        } else {
-            cout << "Insufficient data balance." << endl;
+        cout << "Remaining data: " << dataVolume << " MB" << endl;
+        int usage;
+        cout << "Enter data used (in MB): ";
+        cin >> usage;
+        dataVolume -= usage;
+        if (dataVolume <= 0) {
+            cout << "All data has been used." << endl;
         }
-    }
-
-    void activateData(int volume) {
-        dataVolume = volume;
-        cout << volume << "MB of data activated." << endl;
     }
 };
 
 int main() {
-    DataBundle defaultBundle;
-    defaultBundle.checkData();
-    defaultBundle.useData(200);
-    defaultBundle.checkData();
-
-    cout << "\n How many MB do you want to activate:" << endl;
-    int activatedAmount;
-    cin >> activatedAmount;
-
-    DataBundle customBundle;
-    customBundle.activateData(activatedAmount);
-
-    int dataUsed = 3;
-    customBundle.useData(dataUsed);
-    customBundle.checkData();
-
+    cout << "Choose a data bundle (in MB):" << endl;
+    cout << "1. 500" << endl;
+    cout << "2. 1000" << endl;
+    cout << "3. 2000" << endl;
+    int choice;
+    cin >> choice;
+    int selectedVolume;
+    if (choice == 1) {
+        selectedVolume = 500;
+    } else if (choice == 2) {
+        selectedVolume = 1000;
+    } else if (choice == 3) {
+        selectedVolume = 2000;
+    } else {
+        cout << "Invalid choice, using default 500 MB." << endl;
+        selectedVolume = 500;
+    }
+    DataBundle myBundle(selectedVolume);
+    myBundle.checkData();
+    myBundle.checkData();
     return 0;
 }
